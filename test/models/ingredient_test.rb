03b8e71f204942cf90c56842d6a834b613b7d345
NPhilG <neilphil7@gmail.com>
2020-05-04 23:42:21 -0400

    for ingredient and menu_item test.rb

--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -4,4 +4,38 @@ class IngredientTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+#Testing by Neil Phillip Gloria
+
+  test "Ingredient Creation" do
+    Ingredient.create(id: "21", name: "Ketchup", amount: 15)
+    x = Ingredient.last
+    assert x.id == "21" && x.name == "Ketchup" && x.amount == 15
+  end
+
+
+  test "Ingredient Count" do
+    Ingredient.create(id: "21", name: "Ketchup", amount: 15)
+    x = Ingredient.count
+    assert x.amount == 15
+  end
+
+
+  test "Ingredient Deletion" do
+    Ingredient.create(id: "21", name: "Ketchup", amount: 15)
+    x = Ingredient.last
+    x.destroy
+    assert !Ingredient.find(ingredient).present?
+  end
+
+
+  test "Ingredient Edit" do
+    Ingredient.create(id: "21", name: "Ketchup", amount: 15)
+    x = Ingredient.find_by(id: Ingredient.id)
+    x.name = "Mustard"
+    x.amount = 12
+    x.save(:validate => false)
+    assert x.name == "Mustard" && x.amount == 12 && x.id == 21
+  end
+
 end
--- test/models/menu_item_test.rb
+++ test/models/menu_item_test.rb
@@ -4,4 +4,32 @@ class MenuItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Menu Item Creation" do
+    Ingredient.create(id: "21", name: "Ketchup", amount: 15)
+    Menu_Item.create(ingredients_id: [2,3,4,21], menu_item_id: 32, menu_item_name: "Ham and Turkey Sandwich")
+    x = Menu_Item.last
+    assert x.ingredient_id == [2,3,4,21] && x.menu_item_id == 32 && x.menu_item_name == "Ham and Turkey Sandwich"
+  end
+
+
+  test "Menu Item Deletion" do
+    Ingredient.create(id: "21", name: "Ketchup", amount: 15)
+    Menu_Item.create(ingredients_id: [2,3,4,21], menu_item_id: 32, menu_item_name: "Ham and Turkey Sandwich")
+    x = Menu_Item.last
+    x.destroy
+    assert !Menu_Item.find(menu_item).present?
+  end
+
+
+  test "Menu Item Edit" do
+    Ingredient.create(id: "21", name: "Ketchup", amount: 15)
+    Menu_Item.create(ingredients_id: [2,3,4,21], menu_item_id: 32, menu_item_name: "Ham and Turkey Sandwich")
+    x = Menu_Item.find_by(id: Menu_Item.id)
+    x.menu_item_name = "Turkey with gravy"
+    x.ingredients_id = [2,5]
+    x.save(:validate => false)
+    assert x.menu_item_name == "Turkey with gravy" && x.ingredients_id == [2,5]
+  end
+
 end
