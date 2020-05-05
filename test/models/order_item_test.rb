43cdcb018e92d1f524fce5e1a28cd9b38c0c0d8a
JYL <jyl7094@gmail.com>
2020-05-04 23:03:42 -0400

    order and order items unit testings complete

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -4,4 +4,29 @@ class OrderItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # Testing by Jea Yong Lee
+  test "Order Item creation" do
+    OrderItems.create(note: "Mayo Sandwhich", order_id: 1)
+    order_item = OrderItems.last
+    assert order_item.name == "Mayo Sandwhich" && order_item.order_id == 1
+  end
+
+  test "Order Item edit" do
+    OrderItems.create(note: "Mayo Sandwhich", order_id: 2)
+    order_item = OrderItems.last
+    assert order_item.name == "Mayo Sandwhich" && order_item.order_id == 2
+    edited = OrderItems.find_by(order_id: order_item.order_id)
+    edited.name = "Chicken & Gravy"
+    edited.save(:validate => false)
+    assert order_item.name == "Chicken & Gravy" && order_item.order_id == 2
+  end
+
+  test "Order Item deletion" do
+    OrderItems.create(name: "Salted Ham", order_id: 3)
+    order_num = OrderItems.last.order_id
+    OrderItems.last.destroy
+    assert !OrderItems.find(order_num).present?
+  end
+
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,4 +4,31 @@ class OrderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # Testing by Jea Yong Lee
+
+  test "Order creation" do
+    Orders.create(note: "Mayo Sandwhich", total: 2)
+    order = Orders.last
+    assert order.name == "Mayo Sandwhich" && order.total = 2
+  end
+
+  test "Order edit" do
+    Orders.create(note: "Salted Ham", total: 15)
+    order = Orders.last
+    assert order.name == "Salted Ham" && order.total == 15
+    edited = Orders.find_by(total: order.total)
+    edited.name = "Chicken & Gravy"
+    edited.total = 10
+    edited.save(:validate => false)
+    assert order.name == "Chicken & Gravy" && order.total == 10
+  end
+
+  test "Order deletion" do
+    Orders.create(name: "Turkey Sandwhich", total: 20)
+    order = Orders.last.order_id
+    Orders.last.destroy
+    assert !Order.find(order_num).present?
+  end
+
 end
